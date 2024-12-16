09713cb324c2b011305421f33c36bd38255fa815
LIN JIAN CYUAN <41243122@nfu.edu.tw>
2024-12-16 22:29:02 +0800

    Add files via upload

--- untitled1/mainwindow.h
+++ untitled1/mainwindow.h
@@ -2,13 +2,25 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QString>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
 #include "ui_untitled.h"
-class MainWindow : public QMainWindow
+
+class MainWindow : public QMainWindow,
+                   public Ui_MainWindow
 {
     Q_OBJECT
+private:
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+private slots:
+    void savefile();
+    void close();
 };
+
 #endif // MAINWINDOW_H
